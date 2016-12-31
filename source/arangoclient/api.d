f245367550dc7d673a7d8538710cdcdb6868a496
ReneZwanenburg <renezwanenburg@gmail.com>
2016-12-31 20:34:45 +0100

    Collection info

--- source/arangoclient/api.d
+++ source/arangoclient/api.d
@@ -35,20 +35,6 @@ interface DatabaseAPI
 		bool isSystem;
 	}
 	
-	Result!CurrentDatabase
-	current();
-	
-	
-	alias Databases = string[];
-	
-	Result!Databases
-	user();
-	
-	
-	Result!Databases
-	get();
-	
-	
 	static struct User
 	{
 		string name;
@@ -56,6 +42,19 @@ interface DatabaseAPI
 		bool active;
 		Json extra;
 	}
+	
+	alias Databases = string[];
+	
+	@method(HTTPMethod.GET)
+	Result!CurrentDatabase
+	current();
+	
+	@method(HTTPMethod.GET)
+	Result!Databases
+	user();
+	
+	Result!Databases
+	get();
 	
 	Result!bool
 	create(string name, User[] users);
@@ -77,27 +76,153 @@ interface CollectionAPI
 		Traditional = "traditional",
 		AutoIncrement = "autoincrement"
 	}
-
-	static struct KeyOptions
-	{
-		bool allowUserKeys;
-		KeyType type;
-		int increment;
-		int offset;
-	}
 	
-	static struct CreateResult
+	static
 	{
-		string id;
-		string name;
-		bool waitForSync;
-		bool isVolatile;
-		bool isSystem;
-		int status;
-		CollectionType type;
-		bool error;
-		ushort code;
+		struct KeyOptions
+		{
+			bool allowUserKeys;
+			KeyType type;
+			
+			@optional:
+			int increment;
+			int offset;
+		}
+		
+		struct CreateResult
+		{
+			string id;
+			string name;
+			bool waitForSync;
+			bool isVolatile;
+			bool isSystem;
+			int status;
+			CollectionType type;
+			
+			mixin Status;
+		}
+		
+		struct DeleteResult
+		{
+			string id;
+			
+			mixin Status;
+		}
+		
+		struct Info
+		{
+			mixin InfoTemplate;
+		}
+		
+		struct Properties
+		{
+			mixin PropertiesTemplate;
+		}
+		
+		struct Count
+		{
+			mixin CountTemplate;
+		}
+		
+		struct Figures
+		{
+			mixin CountTemplate;
+			FiguresInfo figures;
+		}
+		
+		struct FiguresInfo
+		{
+			static
+			{
+				struct Indexes
+				{
+					ulong count;
+					ulong size;
+				}
+				
+				struct CompactionStatus
+				{
+					string message;
+					string time;
+				}
+				
+				struct ReadCache
+				{
+					ulong count;
+					ulong size;
+				}
+				
+				struct Alive
+				{
+					ulong count;
+					ulong size;
+				}
+				
+				struct Dead
+				{
+					ulong count;
+					ulong size;
+					ulong deletion;
+				}
+				
+				struct DataFiles
+				{
+					ulong count;
+					ulong fileSize;
+				}
+				
+				struct Journals
+				{
+					ulong count;
+					ulong fileSize;
+				}
+				
+				struct Compactors
+				{
+					ulong count;
+					ulong fileSize;
+				}
+				
+				struct Revisions
+				{
+					ulong count;
+					ulong size;
+				}
+			}
+			
+			Indexes indexes;
+			CompactionStatus compactionStatus;
+			ReadCache readCache;
+			Alive alive;
+			Dead dead;
+			DataFiles datafiles;
+			Journals journals;
+			Compactors compactors;
+			Revisions revisions;
+			ulong lastTick;
+			ulong uncollectedLogfileEntries;
+			ulong documentReferences;
+			string waitingFor;
+		}
+		
+		struct Revision
+		{
+			mixin RevisionTemplate;
+		}
+		
+		struct Checksum
+		{
+			mixin RevisionTemplate;
+			
+			string checksum;
+		}
+		
+		struct CollectionInfo
+		{
+			mixin BasicInfoTemplate;
+		}
 	}
+
 	
 	CreateResult
 	create(string name, CollectionType type);
@@ -108,17 +233,9 @@ interface CollectionAPI
 	CreateResult
 	create(
 		string name, CollectionType type, KeyOptions keyOptions, ulong journalSize,
-		int replicationFactor, bool waitforSync, bool doCompact, bool isVolatile,
+		int replicationFactor, bool waitForSync, bool doCompact, bool isVolatile,
 		string[] shardKeys, int numberOfShards, bool isSystem, int indexBuckets
 	);
-	
-	
-	static struct DeleteResult
-	{
-		string id;
-		bool error;
-		ushort code;
-	}
 	
 	@path(":name")
 	DeleteResult
@@ -129,27 +246,104 @@ interface CollectionAPI
 	DeleteResult
 	delete_(string _name, bool isSystem);
 	
-	
-	static struct CollectionInfo
-	{
-		string id;
-		string name;
-		bool isSystem;
-		int status;
-		CollectionType type;
-		bool error;
-		ushort code;
-	}
-	
 	@path(":name/truncate")
 	@method(HTTPMethod.PUT)
-	CollectionInfo
+	Info
 	truncate(string _name);
+	
+	@path(":name")
+	Info
+	get(string _name);
+	
+	@path(":name/properties")
+	@method(HTTPMethod.GET)
+	Properties
+	properties(string _name);
+	
+	@path(":name/count")
+	@method(HTTPMethod.GET)
+	Count
+	count(string _name);
+	
+	@path(":name/figures")
+	@method(HTTPMethod.GET)
+	Figures
+	figures(string _name);
+	
+	@path(":name/revision")
+	@method(HTTPMethod.GET)
+	Revision
+	revision(string _name);
+	
+	@path(":name/checksum")
+	@method(HTTPMethod.GET)
+	@queryParam("withRevisions", "withRevisions")
+	@queryParam("withData", "withData")
+	Checksum
+	checksum(string _name, bool withRevisions = false, bool withData = false);
+	
+	Result!(CollectionInfo[])
+	get();
+	
+	private
+	{
+		mixin template BasicInfoTemplate()
+		{
+			string id;
+			string name;
+			bool isSystem;
+			int status;
+			CollectionType type;
+		}
+	
+		mixin template InfoTemplate()
+		{
+			mixin BasicInfoTemplate;
+			mixin Status;
+		}
+		
+		mixin template PropertiesTemplate()
+		{
+			mixin InfoTemplate;
+		
+			bool waitForSync;
+			bool doCompact;
+			bool isVolatile;
+			ulong journalSize;
+			KeyOptions keyOptions;
+			
+			@optional
+			{
+				int replicationFactor;
+				int numberOfShards;
+				string[] shardKeys;
+			}
+		}
+		
+		mixin template CountTemplate()
+		{
+			mixin PropertiesTemplate;
+			
+			ulong count;
+		}
+		
+		mixin template RevisionTemplate()
+		{
+			mixin InfoTemplate;
+			
+			string revision;
+		}
+	}
+}
+
+mixin template Status()
+{
+	bool error;
+	ushort code;
 }
 
 struct Result(T)
 {
 	T result;
-	bool error;
-	ushort code;
+	mixin Status;
 }
